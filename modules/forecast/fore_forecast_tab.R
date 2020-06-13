a67312448fd536b4c8ff0ce045fea75347ef013e
Carl Walenciak <cwalenciak@gmail.com>
2020-06-13 09:00:27 -0500

    updated forecast tab to better display forecast vs obs

--- modules/forecast/fore_forecast_tab.R
+++ modules/forecast/fore_forecast_tab.R
@@ -4,11 +4,23 @@ fore_forecast_tab_ui <- function(id){
     tabPanel(
         "5: Forecast",
         fluidRow(
-            numericInput(
-                ns("n_incr"),
-                "n Ahead:",
-                10,
-                min = 5
+            column(
+                width = 4,
+                numericInput(
+                    ns("n_incr"),
+                    "n Ahead:",
+                    10,
+                    min = 5
+                )
+            ),
+            column(
+                width = 4,
+                numericInput(
+                    ns("n_obs"),
+                    "Display Observation N:",
+                    20,
+                    min = 5
+                )
             )
         ),
         fluidRow(
@@ -21,15 +33,18 @@ fore_forecast_tab_ui <- function(id){
 fore_forecast_tab_server <- function(input, output, session, dataset, estimates, d_num, s_num){
     
     output$fore_plot <- renderPlot({
-        fore.aruma.wge(
+        f <- fore.aruma.wge(
             dataset(), 
             phi = estimates()$phi, 
             theta = estimates()$theta, 
             d = d_num(), 
             s = s_num(), 
             n.ahead = input$n_incr,
-            limits = F
+            limits = F,
+            plot = F
         )
+        
+        plot_fore_obs_test(tail(dataset(), input$n_obs), f$f)
     })
     
 }
